41001d234a3eb5f29b29e9a649032b64716abc6d
ravikumar1231 <ravikumardevops1231@gmail.com>
2025-04-16 15:56:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-067198c5ae913ba30"
 instance_type = "t2.medium"
 key_name = "AMAZON"
-vpc_security_group_ids = ["alltraffic"]
+vpc_security_group_ids = ["sg-0f549aecad2338331"]
 tags = {
 Name = var.instance_names[count.index]
 }
